354bde03bf9ec88253c7b39dd7d4bcc9181eb5d6
Melvin Tang <mxcodeyard@gmail.com>
2020-11-06 07:13:51 +0800

    Add CORS settings for dev mode

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
+/*
+PROCESS ENV VARIABLES CONFIGURATION LIST:
+(1) process.env.PORT
+(2) process.env.MONGODB_URI
+(3) process.env.JWT_SECRET_KEY
+*/
+
 import express from 'express';
 import mongoose from 'mongoose';
 import { graphqlHTTP } from 'express-graphql';
@@ -15,6 +22,22 @@ import resolvers from './graphql/resolvers';
     const app = express();
     app.use(express.json());
 
+    if (process.env.NODE_ENV === 'development') {
+      app.use((req, res, next) => {
+        res.set({
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+          'Access-Control-Allow-Headers': ['Authorization', 'Content-Type'],
+          'Access-Control-Allow-Methods': ['OPTIONS', 'GET', 'POST'],
+          'Content-Type': 'application/json',
+        });
+
+        if (req.method === 'OPTIONS') {
+          return res.sendStatus(200);
+        }
+        next();
+      });
+    }
+
     app.use(
       '/graphql',
       graphqlHTTP({
@@ -28,7 +51,7 @@ import resolvers from './graphql/resolvers';
       res.send('Hello Taskmaster!');
     });
 
-    const port = process.env.PORT || 3010;
+    const port = process.env.PORT;
     app.listen(port, () => console.log(`Server connected @ Port: ${port}`));
   } catch (err) {
     console.error(err);
